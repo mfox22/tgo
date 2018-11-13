7c6105f48eefa0c6b73ef70abaea71b78d2ed124
Michael Fox <mfox@stsci.edu>
2018-11-12 21:33:38 -0500

    Added support for local sessions

--- tgo.py
+++ tgo.py
@@ -11,7 +11,7 @@ def get_args():
     # Construct Arguments
     parser = argparse.ArgumentParser(description='tgo - tmux session tool')
     parser.add_argument('--session_file', '-s', required = False, help='The session description file')
-    parser.add_argument('--user', '-u', required = False, help='The user for the session')
+    parser.add_argument('--user', '-u', required = False, default='mfox', help='The user for the session')
     parser.add_argument('--debug', action='store_true', help='Turn on debugging')
     args = parser.parse_args()
     return args
@@ -20,9 +20,9 @@ def process_json(session, user):
     first_time = 0
     windows_present = 0
 
-    json_data = json.load(open(session))
 
     if os.system("tmux has-session -t %s" % (session)):
+        json_data = json.load(open(session))
         os.system("tmux -2 new-session -d -s %s" % (session))
         os.system("tmux set-window-option -t %s -g automatic-rename off" % (session))
         if 'windows' in json_data:
@@ -34,8 +34,24 @@ def process_json(session, user):
                     os.system("tmux new-window  -t %s:1 -k -n '%s' 'ssh %s@%s'" % (session, name, user, node))
                     first_time = 1
                 else:
-                    os.system("tmux new-window  -t %s -n '%s' 'ssh %s@%s'" % (session, name, user, node))
+                    first_time = first_time + 1
+                    os.system("tmux new-window  -t %s:%s -n '%s' 'ssh %s@%s'" % (session, first_time, name, user, node))
         first_time = 0
+
+        if 'local-windows' in json_data:
+            json_data = json.load(open(session))
+            windows_present = 1
+            windows = json_data['local-windows']
+            for name in windows:
+                node = windows[name]
+                if first_time == 0:
+                    os.system("tmux new-window  -t %s:1 -k -n '%s' 'cd %s;bash'" % (session, name, node))
+                    first_time = 1
+                else:
+                    first_time = first_time + 1
+                    os.system("tmux new-window  -t %s:%s -n '%s' 'cd %s;bash'" % (session, first_time, name, node))
+        first_time = 0
+
 
         if 'sync-panes' in json_data:
             panes = json_data['sync-panes']
